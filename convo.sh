e8796b78cf17ba3a17d4bdd48cf5bd82fc52d9c9
kirannaidu7446 <kirannaidu7446@gmail.com>
2026-02-11 20:34:21 +0530

    added conversation b/w john,jk,naresh,luchesh

---
+++ convo.sh
@@ -0,0 +1,9 @@
+ #!/bin/bash
+
+
+echo "john::hii john how are you bachee"
+echo "jk:: hii iam fine how about you bache" 
+echo "naresh::everything going alright bachee??"
+echo "suresh::sare ra bachee ?."
+
+#this about bache luche cincept in shell scripting 

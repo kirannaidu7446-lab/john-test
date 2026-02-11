d9f02bb245a5e934a9818d2759e3a2e5fbedd9b0
kirannaidu7446 <kirannaidu7446@gmail.com>
2026-02-11 22:05:14 +0530

    convo.sh

--- convo.sh
+++ convo.sh
@@ -4,9 +4,9 @@ g2=$2
 g3=$3
 
 
-echo "$g1::hii $g1 how are you bachee"
-echo "$g2::hii $g1 iam fine how about you bache" 
-echo "$g3:: everything $g2 going alright bachee??"
-echo "$g3::hello,Hii bro $g2 sare ra bachee ?."
+echo "${g1}::hii $g1 how are you bachee"
+echo "${g2}::hii $g1 iam fine how about you bache" 
+echo "${g3}:: everything $g2 going alright bachee??"
+echo "${g3}::hello,Hii bro $g2 sare ra bachee ?."
 
 #this about bache luche cincept in shell scripting 

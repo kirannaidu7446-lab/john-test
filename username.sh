af148a5bad301d424ffc438cd2a3d881a896c0f7
kirannaidu7446 <kirannaidu7446@gmail.com>
2026-02-11 22:19:17 +0530

    convo.sh

---
+++ username.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "please enter ur username::"
+read -s username
+
+echo "username entered is ::$username"
+
+echo "enter the password bhayya::"
+
+read -s password
+
+echo "entered password is ::$password"

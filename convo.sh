99e2bf2416f747410adf92f9de0691fb8f349fa3
kirannaidu7446 <kirannaidu7446@gmail.com>
2026-02-11 21:47:44 +0530

    added variables logic

--- convo.sh
+++ convo.sh
@@ -5,7 +5,7 @@ g3='suresh'
 
 
 echo "$g1::hii $g1 how are you bachee"
-echo "$g2: $g1 hii iam fine how about you bache" 
+echo "$g2:: $g1 hii iam fine how about you bache" 
 echo "$g3::$g2 everything going alright bachee??"
 echo "$g3::$g2 sare ra bachee ?."
 
